8683503aabb6e3c73b535ab5baf0da67a5b5bcda
46112 <duf325@naver.com>
2024-01-24 10:38:23 +0900

    init: ì: 초기프로젝트설정

---
+++ src/main/java/com/gonggun/onAir/OnAirApplication.java
@@ -0,0 +1,13 @@
+package com.gonggun.onAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnAirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonggun/onAir/OnAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonggun.onAir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
